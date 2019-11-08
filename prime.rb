a25c15b1dd1ad9a5d620b948696781117c9f48a6
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-08 17:04:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,9 @@ def prime?(num)
   if num <= 1
     return false
   end
-  return false if (2..num).select { |int| num % int  == 0 }
+  if (2..num).select { |int| num % int  == 0 }
+    return false
+  else
+    return true
+  end
 end
