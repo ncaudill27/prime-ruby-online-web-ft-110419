b4e29c883e48a6527de2f9fc48413798363834c7
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-08 17:21:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,11 @@ def prime?(num)
   if num <= 1
     return false
   end
-  if num == 2
+  if num == 2 || num == 3
     return true
   elsif (2..num).select { |int| num % int  == 0 }
-    return true
-  else
     return false
+  else
+    return true
   end
 end
