78bcddbe9a8a3cb351c7666bda3068050511316c
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-08 17:35:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(num)
     if num <= 1
       return false
-    end
-    if (2..num-1).to_a.all? { |int| num % int  != 0 }
+    else
+      (2..num-1).to_a.all? { |int| num % int  != 0 }
     end
 end
